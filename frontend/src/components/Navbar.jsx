36b62a24da32c4f62b418eac1dee22c5053b7933
Muttanna Singadi <muttufs767@gmail.com>
2026-03-20 15:29:26 +0530

    Added Admin Dashboard UI with styles and updated routing

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -25,7 +25,7 @@ export default function Navbar() {
       <ul className={menuOpen ? "nav-links active" : "nav-links"}>
         <li><Link to="/">Home</Link></li>
        <li><Link to="/searchcertificate">Verify</Link></li>
-       <li><a href="/admin">Admin</a></li>
+       <li><Link to="/admindashboard">Admin</Link></li>
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/register">Register</Link></li>
       </ul>
