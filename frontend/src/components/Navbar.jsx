da43dcb1dfa50b54db4f3c003f37c702c774dc91
Muttanna Singadi <muttufs767@gmail.com>
2026-03-26 17:35:10 +0530

    Updated Navbar and moved CSS to styles folder

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
-import "./navbar.css";
+import "../styles/navbar.css";
 
 export default function Navbar() {
 
